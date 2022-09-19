1ea5517ccee4bcac7bb8369af04fdcff4cd8b652
saskiabt <sskbinder@gmail.com>
2022-09-18 21:11:04 -0400

    add update post and delete post controllers

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -8,6 +8,7 @@ const getPost = asyncHandler(async (req, res) => {
     res.status(200).json({ posts });
   } catch (err) {
     if (err) console.log(err.message);
+    process.exit();
   }
 });
 
@@ -30,19 +31,42 @@ const createPost = asyncHandler(async (req, res) => {
 
 const updatePost = asyncHandler(async (req, res) => {
   try {
-    res.status(200).json({ message: `updated post: ${req.params.id}` });
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      res.status(400);
+      throw new Error("Post not found");
+    }
+
+    const updatedPost = await Post.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
+    res.status(200).json({ message: `updated post: ${updatedPost}` });
   } catch (err) {
     if (err) console.log(`Error in updatePost: ${err}`);
+    process.exit();
   }
 });
 
 const deletePost = asyncHandler(async (req, res) => {
   try {
-    res.status(200).json({ message: `updated post: ${req.params.id}` });
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      res.status(400);
+      throw new Error("Post not found");
+    }
+
+    const deletedPost = await Post.findByIdAndDelete(req.params.id);
+
+    res
+      .status(200)
+      .json({ message: `successfully deleted post: ${deletedPost}` });
   } catch (err) {
-    if (err) console.log("Error in updatePost");
+    if (err) console.log(`Error in updatePost: ${err}`);
+    process.exit();
   }
-  res.status(200).json({ message: `deletex post: ${req.params.id}` });
 });
 
 module.exports = {
