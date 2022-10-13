13f4ce5d9060cca61497d5b1ff1d23a0c2f43d88
saskiabt <sskbinder@gmail.com>
2022-10-13 13:03:34 -0400

    basic formatting for spinner

--- frontend/src/components/spinner/Spinner.jsx
+++ frontend/src/components/spinner/Spinner.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import "../spinner/Spinner.css";
 
 function Spinner() {
   return (
