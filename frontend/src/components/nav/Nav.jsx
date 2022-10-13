d0caad18a30c1b8977c3ca3919b696ddee1e3363
saskiabt <sskbinder@gmail.com>
2022-10-13 09:54:46 -0400

    boilerplate code for nav

--- frontend/src/components/nav/Nav.jsx
+++ frontend/src/components/nav/Nav.jsx
@@ -3,16 +3,15 @@ import { Link, redirect, useNavigate } from "react-router-dom";
 import "./styles/nav.css";
 import "../../icons/menu-icon.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faX, faHome } from "@fortawesome/free-solid-svg-icons";
-import DarkModeButton from "../dark-mode-button/DarkModeButton";
+import { faHome, faBars } from "@fortawesome/free-solid-svg-icons";
 import { useSelector, useDispatch } from "react-redux";
 import { logout, reset } from "../../features/auth/authSlice";
 import "../../styles/darkMode.css";
+
 function Nav() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { user } = useSelector((state) => state.auth);
-  const [isHidden, setIsHidden] = useState(false);
 
   const onLogout = () => {
     dispatch(logout());
@@ -20,7 +19,29 @@ function Nav() {
     navigate("/");
     console.log("navigated!");
   };
+
+  const [navDisplay, setNavDisplay] = useState({ display: "none" });
+
+  const toggleNav = () => {
+    console.log("listening");
+    navDisplay === { display: "none" }
+      ? setNavDisplay({ display: "flex" })
+      : setNavDisplay({ display: "none" });
+    console.log(navDisplay);
+  };
   return (
+    // <div className="nav-container">
+    //   <div className="nav-mobile">
+    //     <button
+    //       type="button"
+    //       className="nav-btn"
+    //       id="nav-toggle"
+    //       onClick={toggleNav}
+    //     >
+    //       <FontAwesomeIcon icon={faBars} />
+    //     </button>
+    //   </div>
+    //   <div className="nav-visibility" style={navDisplay}>
     <div className="Nav">
       <div className="nav-left">
         <Link to="/" className="nav-icon nav-icon-home">
@@ -63,6 +84,8 @@ function Nav() {
         </div>
       )}
     </div>
+    //   </div>
+    // </div>
   );
 }
 
