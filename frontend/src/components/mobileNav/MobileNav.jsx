18d8ec6233087eff30efb16a74f523f57c84b0ab
saskiabt <sskbinder@gmail.com>
2022-11-12 13:23:27 -0500

    fix routing and conditional rendering of login / dashboard buttons on mobile dropdown menu

--- frontend/src/components/mobileNav/MobileNav.jsx
+++ frontend/src/components/mobileNav/MobileNav.jsx
@@ -1,15 +1,28 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser, faCaretSquareDown } from "@fortawesome/free-regular-svg-icons";
 import { useNavigate } from "react-router-dom";
 import "./MobileNav.css";
 import { faRss } from "@fortawesome/free-solid-svg-icons";
+import { logout, reset } from "../../features/auth/authSlice";
 
 function MobileNav({ isExpanded, setIsExpanded }) {
+  const { user } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
   const navigate = useNavigate();
   const toggleDropDown = () => {
     setIsExpanded(!isExpanded);
   };
+
+  const onLogout = () => {
+    dispatch(logout());
+    dispatch(reset());
+    navigate("/");
+    console.log("navigated!");
+  };
+
   return (
     <div className="MobileNav">
       <div className="mobile-horizontal">
@@ -18,11 +31,11 @@ function MobileNav({ isExpanded, setIsExpanded }) {
         </button>
         <button type="button" onClick={toggleDropDown}>
           <FontAwesomeIcon icon={faUser} />
-          <FontAwesomeIcon icon={faCaretSquareDown} />
+          {!user ? <FontAwesomeIcon icon={faCaretSquareDown} /> : null}
         </button>
       </div>
 
-      {isExpanded && (
+      {!user && isExpanded && (
         <div className="mobile-dropdown">
           <button
             type="button"
@@ -33,6 +46,20 @@ function MobileNav({ isExpanded, setIsExpanded }) {
           </button>
           <button type="button" onClick={() => navigate("/register")}>
             Register
+          </button>
+        </div>
+      )}
+      {isExpanded && user && (
+        <div className="mobile-dropdown">
+          <button
+            type="button"
+            id="mobile-login"
+            onClick={() => navigate("/dashboard")}
+          >
+            My Posts
+          </button>
+          <button type="button" onClick={onLogout}>
+            Logout
           </button>
         </div>
       )}
